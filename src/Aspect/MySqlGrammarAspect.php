ca63da92268a71b588acc40906bc65008f38e703
huangdijia <huangdijia@gmail.com>
2022-07-16 13:09:31 +0800

    first commit

---
+++ src/Aspect/MySqlGrammarAspect.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/mysql-grammar-addon.
+ *
+ * @link     https://github.com/friendsofhyperf/mysql-grammar-addon
+ * @document https://github.com/friendsofhyperf/mysql-grammar-addon/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\MySqlGrammarAddon\Aspect;
+
+use Hyperf\Di\Annotation\Aspect;
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+
+#[Aspect()]
+class MySqlGrammarAspect extends AbstractAspect
+{
+    public array $classes = [
+        'Hyperf\Database\Schema\Grammars\MySqlGrammar::compileColumnListing',
+        'Hyperf\Database\Schema\Grammars\MySqlGrammar::compileColumns',
+    ];
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        return match ($proceedingJoinPoint->getReflectMethod()->getName()) {
+            'compileColumnListing' => str_replace(
+                ', `column_comment` as `column_comment`,',
+                ', binary `column_comment` as `column_comment`,',
+                $proceedingJoinPoint->process()
+            ),
+            'compileColumns' => str_replace(
+                ', `column_comment`',
+                ', binary `column_comment`',
+                $proceedingJoinPoint->process()
+            ),
+            default => $proceedingJoinPoint->process()
+        };
+    }
+}
